498ff62c256843ee94c5c16844622863181bf5af
Bizzzzan <159256857+Bizzzzan@users.noreply.github.com>
2025-04-10 17:13:33 +0100

    Update and rename create_assignment_table_v1.sql to create_assignment_table_v2.sql

--- create_assignment_table_v2.sql
+++ create_assignment_table_v2.sql
@@ -1,9 +1,8 @@
 CREATE TABLE assignment (
 	assignment_id BIGINT AUTO_INCREMENT PRIMARY KEY, 
 	assignment_name VARCHAR(20) NOT NULL,
-    teacher_id MEDIUMINT NOT NULL,
+    user_id BIGINT NOT NULL,
     submitted_date DATETIME NOT NULL,
-    FOREIGN KEY(teacher_id) REFERENCES teacher_details(teacher_id)
+    FOREIGN KEY(user_id) REFERENCES users(user_id)
 );
 SELECT * FROM assignment;
-
