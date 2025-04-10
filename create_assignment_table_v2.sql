4efcc1c0f2a5e41523f4b130b11062ced093848e
Bizzzzan <159256857+Bizzzzan@users.noreply.github.com>
2025-04-10 17:30:43 +0100

    Delete create_assignment_table_v2.sql

--- create_assignment_table_v2.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE assignment (
-	assignment_id BIGINT AUTO_INCREMENT PRIMARY KEY, 
-	assignment_name VARCHAR(20) NOT NULL,
-    user_id BIGINT NOT NULL,
-    submitted_date DATETIME NOT NULL,
-    FOREIGN KEY(user_id) REFERENCES users(user_id)
-);
-SELECT * FROM assignment;
