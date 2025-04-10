f4106cc600614beb46659bfd262291ab4d15a3f0
Bizzzzan <159256857+Bizzzzan@users.noreply.github.com>
2025-04-10 17:30:51 +0100

    Delete create_customisation_table_v1.sql

--- create_customisation_table_v1.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE customisation (
-	student_id INT PRIMARY KEY, 
-    profile_pic BLOB, 
-    profile_background_image MEDIUMBLOB, 
-	profile_bio VARCHAR(625), 
-	FOREIGN KEY(student_id) REFERENCES student_details(student_id)
-);
-SELECT * FROM customisation;
