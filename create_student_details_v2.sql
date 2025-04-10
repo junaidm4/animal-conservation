27c0501449ada2eff27fe660dd5176636c3c441e
Bizzzzan <159256857+Bizzzzan@users.noreply.github.com>
2025-04-10 17:31:16 +0100

    Delete create_student_details_v2.sql

--- create_student_details_v2.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE students (
-	user_id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    access_code MEDIUMINT NOT NULL UNIQUE, 
-    enrolled_date DATE NOT NULL, 
-    disability VARCHAR(100),
-    FOREIGN KEY(user_id) REFERENCES users(user_id)
-);
-SELECT * FROM students;
