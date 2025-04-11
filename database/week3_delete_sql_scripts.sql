75ccc2f91082dd3773e8c1aded6d3b3bb7c09d8a
Bizzzzan <159256857+Bizzzzan@users.noreply.github.com>
2025-04-11 14:37:34 +0100

    Add files via upload

---
+++ database/week3_delete_sql_scripts.sql
@@ -0,0 +1,14 @@
+DELETE FROM notes;
+DELETE FROM customisation;
+DELETE FROM students_class_teachers;
+DELETE FROM assignment_students;
+DELETE FROM assignment;
+DELETE FROM class_management;
+DELETE FROM teacher_details;
+DELETE FROM student_details;
+
+
+
+
+
+
